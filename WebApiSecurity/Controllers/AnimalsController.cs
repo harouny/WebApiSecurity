7a45343f87a3b6481abd03f3b9d56d0b9a6215ce
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-11-26 22:14:06 +1100

    add loging for user identity

--- WebApiSecurity/Controllers/AnimalsController.cs
+++ WebApiSecurity/Controllers/AnimalsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
@@ -17,19 +18,40 @@ namespace WebApiSecurity.Controllers
         
         public IHttpActionResult Get()
         {
+            LogUser();
             return Ok(_animals);
         }
 
         public IHttpActionResult Put(string animal)
         {
+            LogUser();
             _animals.Add(animal);
             return Ok(animal);
         }
 
         public IHttpActionResult Delete(string animal)
         {
+            LogUser();
             _animals.Remove(animal);
             return Ok(animal);
+        }
+
+        private void LogUser()
+        {
+            var msg = "--------------" + Environment.NewLine;
+
+            if (User != null && User.Identity != null)
+            {
+                msg += "User: " + User.Identity.Name + Environment.NewLine;
+                msg += "AuthType: " + User.Identity.AuthenticationType + Environment.NewLine;
+            }
+            else
+            {
+                msg += "Not Authenticated" + Environment.NewLine;
+            }
+            msg += "--------------" + Environment.NewLine;
+            System.Diagnostics.Debugger.Log(0, GetType().Name, msg);
+            Console.WriteLine(msg);
         }
 
     }
