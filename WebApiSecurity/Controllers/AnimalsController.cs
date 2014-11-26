ad65ad57e2f9ddf507e316c57ae493d6e1a157d2
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-11-26 21:55:02 +1100

    initial setup, web api, owin, demo controller, owin host

---
+++ WebApiSecurity/Controllers/AnimalsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiSecurity.Controllers
+{
+    public class AnimalsController : ApiController
+    {
+        private static List<string> _animals;
+        public AnimalsController()
+        {
+            if (_animals == default (List<string>))
+            {
+                _animals =  (new[] { "cat", "dog", "mouse" }).ToList();
+            }
+        }
+        
+        public IHttpActionResult Get()
+        {
+            return Ok(_animals);
+        }
+
+        public IHttpActionResult Put(string animal)
+        {
+            _animals.Add(animal);
+            return Ok(animal);
+        }
+
+        public IHttpActionResult Delete(string animal)
+        {
+            _animals.Remove(animal);
+            return Ok(animal);
+        }
+
+    }
+}
---
+++ WebApiSecurity/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiSecurity.Startup))]
+
+namespace WebApiSecurity
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            System.Diagnostics.Debugger.Launch();
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            app.UseWebApi(config);
+           
+        }
+    }
+}
